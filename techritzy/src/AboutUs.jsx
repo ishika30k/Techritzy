0ca06e1cad8329d95de7f9e3a239fa66f0c9d487
ishika30k <ishikakh30@gmail.com>
2026-04-08 16:43:39 +0530

    changed about us page

--- techritzy/src/AboutUs.jsx
+++ techritzy/src/AboutUs.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import PageTwo from './assets/Components/pagetwo'
+import Footer from './assets/Components/Footer'
 
 const AboutUs = () => {
   return (
     <div>
+      <h1>
+        We Craft <br />Digital <br />Experiences
+      </h1>
       <PageTwo/>
+      <Footer/>
     </div>
   )
 }
