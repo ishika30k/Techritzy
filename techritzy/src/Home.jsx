9b367a7a1ceac232b8ca6a65546dc18ffc828531
ishika30k <ishikakh30@gmail.com>
2026-04-08 16:45:02 +0530

    changed home.jsx page

--- techritzy/src/Home.jsx
+++ techritzy/src/Home.jsx
@@ -4,20 +4,24 @@ import PageTwo from './assets/Components/pagetwo'
 import PageThree from './assets/Components/pagethree'
 import Card from './assets/Components/Card'
 import {info} from './assets/Components/constants'
+import ContactCard from './assets/Components/ContactCard'
+import { contact } from './assets/Components/constants'
+import ContactUs from './assets/Components/contactUs'
+import Footer from './assets/Components/Footer'
 
 const Home = () => {
   return (
     <div className='bg-black overflow-hidden'>
       <PageOne/>
-      <div className='bg-black flex flex-wrap  m-9 gap-6 justify-around'>
+      <div className='bg-black flex flex-wrap my-16 mx-16 justify-around'>
         {info.map(function(elem,idx){
     return (
      <Card key={idx} heading={elem.heading} description={elem.description} image={elem.image}/>
       )
     })}
-      </div> 
-      <PageTwo/> 
+      </div>  
       <PageThree/>
+      <Footer/>
     </div>
   )
 }
