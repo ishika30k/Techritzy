a13f39d53f9aa4bea9be1ee2c5f7ab75cac7d01d
ishika30k <ishikakh30@gmail.com>
2026-04-12 02:43:19 +0530

    edited a  page

--- techritzy/src/App.jsx
+++ techritzy/src/App.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import Navbar from './assets/Components/Navbar'
 import Home from './Home'
-import { useGSAP } from '@gsap/react'
 import { Route,Routes } from 'react-router-dom'
 import WhatWeDo from './assets/Components/WhatWeDo'
 import ContactUs from './assets/Components/contactUs'
 import AboutUs from './AboutUs'
 import Footer from './assets/Components/Footer'
+import ScrollToTop from './ScrollToTop'
 
 const App = () => {
 
   return (
     <div className='bg-black overflow-hidden'>
       <Navbar/>
+      <>
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/services' element={<WhatWeDo/>} />
+        <Route path='/services/:id' element={<WhatWeDo/>} />
+        <Route path="/services" element={<WhatWeDo />} />
         <Route path='/about' element={<AboutUs/>} />
         <Route path='/contact' element={<ContactUs/>} />
       </Routes>
+      </>
       <Footer/>
     </div>  
   )
