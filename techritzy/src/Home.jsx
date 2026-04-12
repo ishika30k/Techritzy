9d15896070f7d9332a20f40e3a62f363dd6d3359
ishika30k <ishikakh30@gmail.com>
2026-04-12 23:03:28 +0530

    added animations

--- techritzy/src/Home.jsx
+++ techritzy/src/Home.jsx
@@ -5,22 +5,30 @@ import Card from './assets/Components/Card'
 import {info} from './assets/Components/constants'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
 
   useGSAP(() => {
-      gsap.from('.card', {
-        y: 150,
-        opacity: 0,
-        duration: 2,
-        ease: "power3.out"
-      })
+    gsap.from('.card', {
+      y: 100,
+      opacity: 0,
+      duration: 0.8,
+      stagger: 0.3,  
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: '.card-container', 
+        start: 'top 70%',
+      }
     })
+  })
 
   return (
     <div className='bg-black overflow-hidden'>
       <PageOne/>
-      <div className='card heading-font bg-black flex flex-wrap justify-center gap-6 px-2 py-10'>
+      <div className='card-container heading-font bg-black flex flex-wrap justify-center gap-6 px-2 py-10'>
         {info.map(function(elem,idx){
     return (
      <Card key={idx}  id={idx} heading={elem.heading} description={elem.description} image={elem.image}/>
