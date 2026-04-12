6ec1676fb027f978fb97e169eb6fc117aadaabce
ishika30k <ishikakh30@gmail.com>
2026-04-13 00:16:50 +0530

    added deployment issue solution

--- techritzy/src/assets/Components/ContactCard.jsx
+++ techritzy/src/assets/Components/ContactCard.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const ContactCard = (props) => {
   return (
-    <div className='card 
+    <a 
+      href={props.link}
+      className='card 
       h-auto lg:h-80 md:h-70
       w-full max-w-xs sm:max-w-sm md:max-w-md lg:w-80 
       m-3 sm:m-5 
       bg-blue-950 
       hover:scale-105 hover:shadow-[0_0_30px_rgba(59,130,246,0.5)] hover:-translate-y-3 
       border-white rounded-2xl 
-      flex flex-col items-start'>
+      flex flex-col items-start'
+    >
 
       <img 
         className='h-8 sm:h-10 rounded-4xl m-4 sm:m-5' 
@@ -25,7 +28,7 @@ const ContactCard = (props) => {
         {props.info}
       </h3>
 
-    </div>
+    </a>
   )
 }
 
